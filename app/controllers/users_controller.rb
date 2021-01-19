0c25bef150c3e13932723b1ec00a9630c436927e
Henok <henatan99@gmail.com>
2021-01-19 19:57:11 +0300

    Adds edit and update actions to the UsersController Adds form helpers to new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,21 @@ class UsersController < ApplicationController
     end
 
     def new
+      @user = User.new
+    end
+
+    def edit 
+      @user = User.find(params[:id])
+    end 
+
+    def update
+      @user = User.find(params[:id])
+       
+      if @user.update(user_params)
+        redirect_to @user
+      else 
+        render :edit 
+      end 
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "users#index"
-  resources :users, :only => [:new, :create]
+  # root "users#index"
+  resources :users, :only => [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
