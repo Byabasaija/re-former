2ddee6000ee0b7bb28efc197d706d7ea261a08d0
Your Name <basaijapascal9@gmail.com>
2021-01-19 14:05:48 +0300

    create users controler,routes and html form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+          redirect_to new_user_path
+        else
+        render :new
+        end
+    end
+
+    def new
+    end
+
+    private
+    def user_params
+     params.require(:user).permit(:username, :email, :password)
+    end
+    
+end
